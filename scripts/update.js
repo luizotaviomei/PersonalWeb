8244d0e8c24985beadcc765a488262068cfcac39
luizotaviomei <meiradeniz3@gmail.com>
2025-07-01 20:34:54 -0300

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -1,1 +1,12 @@
+document.querySelectorAll(".accordion").forEach(button => {
+  button.addEventListener("click", () => {
+    button.classList.toggle("active");
 
+    const panel = button.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+});

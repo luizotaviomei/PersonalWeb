b35ac870b4ae2351ac878a72fe4499a0665f33e1
luizotaviomei <meiradeniz3@gmail.com>
2025-07-01 16:21:55 -0300

    Delete theme.js

--- theme.js
+++
@@ -1,9 +0,0 @@
-function toggleTheme(checkbox) {
-  if (checkbox.checked) {
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light');
-  } else {
-    document.documentElement.removeAttribute('data-theme');
-    localStorage.setItem('theme', 'dark');
-  }
-}

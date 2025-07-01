2943a3dbc211bd9dd69e6addb9dd1ef1ec2fc2e6
luizotaviomei <meiradeniz3@gmail.com>
2025-07-01 16:29:25 -0300

    Update modalUpdates.js

--- scripts/modalUpdates.js
+++ scripts/modalUpdates.js
@@ -1,1 +1,7 @@
-// Conteúdo de openUpdates, closeUpdates e showVersion irá aqui.
+function openUpdates() {
+  document.getElementById("updates-modal").classList.remove("hidden");
+}
+
+function closeUpdates() {
+  document.getElementById("updates-modal").classList.add("hidden");
+}// Conteúdo de openUpdates, closeUpdates e showVersion irá aqui.

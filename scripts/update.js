79950e90b8f7d5be1f2532e1416f6846f404521a
luizotaviomei <meiradeniz3@gmail.com>
2025-07-01 21:01:54 -0300

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -1,15 +1,8 @@
+// scripts/update.js
 document.querySelectorAll(".accordion-toggle").forEach(button => {
   button.addEventListener("click", () => {
     button.classList.toggle("open");
-
-    const panel = button.nextElementSibling;
-
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-      panel.classList.remove("open");
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-      panel.classList.add("open");
-    }
+    const content = button.nextElementSibling;
+    content.classList.toggle("open");
   });
 });

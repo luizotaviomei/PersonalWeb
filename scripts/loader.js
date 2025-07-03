f8a9e97b245d436ee0b231d133d29e6f08487ac7
luizotaviomei <meiradeniz3@gmail.com>
2025-07-03 14:09:06 -0300

    Update loader.js

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,32 +1,16 @@
+
 function initLoader() {
   window.addEventListener("load", () => {
     const loader = document.getElementById('loader');
-    const typingElement = document.getElementById("typing");
-
-    const isReturning = localStorage.getItem('visitedBefore') === 'true';
-    const welcomeText = isReturning ? "Welcome back to Link Hub..." : "Welcome to Link Hub...";
 
     setTimeout(() => {
-      loader.style.opacity = '0';
-
-      setTimeout(() => {
-        loader.remove();
-        localStorage.setItem('visitedBefore', 'true');
-
-        // Só executa o typing se o elemento existir
-        if (typingElement) {
-          let i = 0;
-          const interval = setInterval(() => {
-            if (i < welcomeText.length) {
-              typingElement.textContent += welcomeText[i];
-              i++;
-            } else {
-              clearInterval(interval);
-            }
-          }, 100);
-        }
-
-      }, 500);
+      if (loader) {
+        loader.style.opacity = '0';
+        setTimeout(() => {
+          loader.remove();
+          localStorage.setItem('visitedBefore', 'true');
+        }, 500);
+      }
     }, 1000);
   });
 }
